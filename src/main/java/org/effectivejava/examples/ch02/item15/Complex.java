dd40c40cfa997300f0e5c2c6a4def74efcf563ed
Prasanth Guruprasad <nagu89@gmail.com>
2015-10-02 23:21:55 -0700

    item15

---
+++ src/main/java/org/effectivejava/examples/ch02/item15/Complex.java
@@ -0,0 +1,62 @@
+package org.effectivejava.examples.ch02.item15;
+
+public final class Complex {
+    private final double re;
+    private final double im;
+
+    public Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double realPart() { return re; }
+
+    public double imaginaryPart() { return im; }
+
+    public Complex add(Complex c) {
+        return new Complex(re + c.re, im + c.im);
+    }
+
+    public Complex subtract(Complex c) {
+        return new Complex(re - c.re, im - c.im);
+    }
+
+    public Complex multiply(Complex c) {
+        return new Complex(re * c.re - im * c.im,
+                            re * c.im + im * c.re);
+    }
+
+    public Complex divide(Complex c) {
+        double tmp = c.re * c.re  + c.im * c.im;
+        return new Complex((re * c.re + im * c.im) / tmp,
+                            (im * c.re - re * c.im) / tmp);
+    }
+
+    @Override public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Complex))
+            return false;
+        else {
+            Complex c = (Complex) o;
+            return Double.compare(re, c.re) == 0
+                    && Double.compare(im, c.im) == 0;
+        }
+    }
+
+    @Override public int hashCode() {
+        int result = 17;
+        result = 31 * result + hashDouble(re);
+        result = 31 * result + hashDouble(im);
+        return result;
+    }
+
+    private static int hashDouble(double d) {
+        long longBits = Double.doubleToLongBits(d);
+        return (int) (longBits ^ (longBits >>> 32));
+    }
+
+    @Override public String toString() {
+        return "(" + re + " + " + im +  "i)";
+    }
+}
